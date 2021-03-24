2b732cbef6c6976ecd3ff588b5adf9ea940f7024
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-03-24 16:35:18 +0100

    Try to get more output on compilation error

--- Benchmarks/runner_simgrid.py
+++ Benchmarks/runner_simgrid.py
@@ -2,16 +2,15 @@ import shutil, os, sys, stat, subprocess, re, argparse, shlex
 
 def run_cmd(buildcmd, execcmd, binary, read_line_lambda=None):
     output = "Compiling https://gitlab.com/MpiCorrectnessBenchmark/mpicorrectnessbenchmark/-/tree/master/Benchmarks/microbenchs/{}.c\n\n".format(binary)
+    output += "$ {}".format(buildcmd)
 
     compil = subprocess.run(buildcmd, shell=True, stderr=subprocess.STDOUT)
     if compil.stdout is not None:
         output += compil.stdout
     if compil.returncode != 0:
-        print("Compilation of {}.c raised an error (retcode: %d)".format(binary, compil.returncode))
-        print("$ {}".format(buildcmd))
-        if compil.stdout is not None:
-            for line in compil.stdout.split('\n'):
-                print(line, end='')
+        output += "Compilation of {}.c raised an error (retcode: %d)".format(binary, compil.returncode)
+        for line in (output.split('\n')):
+            print ("| {}".format(line))
         return 'CUN', compil.returncode, output
 
     output += "\n\nExecuting https://gitlab.com/MpiCorrectnessBenchmark/mpicorrectnessbenchmark/-/tree/master/Benchmarks/microbenchs/{}.c\n\n$ {}\n".format(binary,execcmd)
