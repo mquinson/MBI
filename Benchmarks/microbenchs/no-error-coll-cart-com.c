201aade1f9ccbaebbf4dd43be99c52ae207e3452
Emmanuelle Saillard <emmanuelle.saillard@inria.fr>
2021-03-27 00:00:54 +0100

    fixes a code

--- Benchmarks/microbenchs/no-error-coll-cart-com.c
+++ Benchmarks/microbenchs/no-error-coll-cart-com.c
@@ -1,16 +1,16 @@
 ////////////////// MPI bugs collection header //////////////////
 //
-// Origin: MPICorrectnessBenchmark
+// Origin: MUST
 //
-// Description: Creates a cartesian communicator, and gets cartesian information 
+// Description: Creates a cartesian communicator, and tries to get information with Cart_get without triggering any errors or warnings
 //
 //// List of features
 // P2P: Lacking
 // iP2P: Lacking
 // PERS: Lacking
-// COLL: Incorrect
+// COLL: Correct
 // iCOLL: Lacking
-// TOPO: Incorrect
+// TOPO: Correct
 // IO: Lacking
 // RMA: Lacking
 // PROB: Lacking
@@ -42,22 +42,24 @@
 
 int main(int argc, char** argv)
 {
-  int nprocs = -1;
-  int rank   = -1;
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int namelen = 128;
+
+	int size, rank;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Get_processor_name(processor_name, &namelen);
-  printf("rank %d is alive on %s\n", rank, processor_name);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+ 
+  MPI_Status status;
+ 
 
-  if (nprocs != 2) {
+  if (size < 2) {
     printf("\033[0;31m! This test needs at 2 processes !\033[0;0m\n");
     MPI_Finalize();
     return 1;
   }
+
+  //Say hello
+  printf("Hello, I am rank %d of %d processes\n",rank,size);
 
   // create a cartesian communicator
   MPI_Comm comm;
@@ -68,11 +70,8 @@ int main(int argc, char** argv)
   periods[0] = 1;
   periods[1] = 1;
 
-  printf("This is rank %d\n", rank);
-
   MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm);
   MPI_Cart_get(comm, 2, dims, periods, coords);
-  printf("This is rank %d\n", rank);
 
   if (comm != MPI_COMM_NULL)
     MPI_Comm_free(&comm);
