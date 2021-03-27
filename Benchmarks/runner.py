4d9f3e712b39b4bb496727fde0665ba47c149393
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-03-27 01:44:45 +0100

    Try harder to kill all subprocesses on timeout

--- Benchmarks/runner.py
+++ Benchmarks/runner.py
@@ -29,7 +29,7 @@ def run_cmd(buildcmd, execcmd, binary, timeout, read_line_lambda=None):
         print ("| {}".format(line), file=sys.stderr)
 
     # We run the subprocess and parse its output line by line, so that we can kill it as soon as it detects a timeout
-    process = subprocess.Popen(shlex.split(execcmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process = subprocess.Popen(shlex.split(execcmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
     res = None
     while True:
         line = process.stdout.readline()
@@ -40,7 +40,12 @@ def run_cmd(buildcmd, execcmd, binary, timeout, read_line_lambda=None):
             if read_line_lambda != None:
                 read_line_lambda(line, process)
         if time.time() - start_time > timeout:
+            pid = process.pid
+            os.killpg(os.getpgid(pid), signal.SIGTERM)  # Send the signal to all the processes in the group. The command and everything it forked
+            time.sleep(0.2)
             process.terminate()
+            time.sleep(0.2)
+            os.killpg(os.getpgid(pid), signal.SIGKILL)  # Go for a brutal KILL, just in case
             res = 'timeout'
         if process.poll() is not None:
             break
