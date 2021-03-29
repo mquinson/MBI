840dddc175f87d3c11fc359b8c3091b8e759d296
matlaurent <mathieu.laurent@etudiant.univ-rennes1.fr>
2021-03-29 14:39:13 +0200

    fixed un-updated headers

--- Benchmarks/microbenchs/no-error-coll-cart.c
+++ Benchmarks/microbenchs/no-error-coll-cart.c
@@ -2,15 +2,16 @@
 //
 // Origin: MPICorrectnessBenchmark
 //
-// Description: Creates a cartesian communicator, and creates new communicators for lines and columns 
+// Description: Creates a cartesian communicator, and creates new communicators
+// for lines and columns
 //
 //// List of features
 // P2P: Lacking
 // iP2P: Lacking
 // PERS: Lacking
-// COLL: Incorrect
+// COLL: Correct
 // iCOLL: Lacking
-// TOPO: Incorrect
+// TOPO: Correct
 // IO: Lacking
 // RMA: Lacking
 // PROB: Lacking
@@ -18,10 +19,6 @@
 // GRP: Lacking
 // DATA: Lacking
 // OP: Lacking
-<<<<<<< HEAD
-// HYB: Lacking
-=======
->>>>>>> a32ab45fe0149c6c540578fd7c26f080dcc1780e
 // LOOP:  Lacking
 // SP: Correct
 //
@@ -29,7 +26,7 @@
 // deadlock: never
 // numstab: never
 // segfault: never
-// mpierr: never 
+// mpierr: never
 // resleak: never
 // livelock: never
 // datarace: never
@@ -40,59 +37,62 @@
 ////////////////// End of MPI bugs collection header //////////////////
 //////////////////       original file begins        //////////////////
 
+#include <math.h>
 #include <mpi.h>
 #include <stdio.h>
-#include <math.h>
 
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 
-  int rank, newrank, Rrank, Crank, size, ndim=2, dim, periods[2]={0,0}, reorder=0, coords[2];
-	int remain_dims[2];
-	MPI_Comm newcomm, Rcomm, Ccomm;
+  int rank, newrank, Rrank, Crank, size, ndim = 2, dim, periods[2] = {0, 0},
+                                         reorder = 0, coords[2];
+  int remain_dims[2];
+  MPI_Comm newcomm, Rcomm, Ccomm;
 
-  MPI_Init(&argc,&argv);
+  MPI_Init(&argc, &argv);
 
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	// Define the dimensions of the square grid
-	dim = sqrt(size);
+  // Define the dimensions of the square grid
+  dim = sqrt(size);
 
-	if(rank ==0)
-		fprintf(stderr,"Dimensions of the grid:%d x %d  - %d processes\n", dim, dim, size);	
+  if (rank == 0)
+    fprintf(stderr, "Dimensions of the grid:%d x %d  - %d processes\n", dim,
+            dim, size);
 
-	int dims[2]={dim,dim};
+  int dims[2] = {dim, dim};
 
-	MPI_Cart_create(MPI_COMM_WORLD, ndim, dims, periods, reorder, &newcomm);
+  MPI_Cart_create(MPI_COMM_WORLD, ndim, dims, periods, reorder, &newcomm);
 
-	if(newcomm == MPI_COMM_NULL)
-		fprintf(stderr,"newcomm is null for P%d (process not in the grid)\n",rank);
+  if (newcomm == MPI_COMM_NULL)
+    fprintf(stderr, "newcomm is null for P%d (process not in the grid)\n",
+            rank);
 
-	if(rank < (dim*dim)){
-		MPI_Comm_rank(newcomm, &newrank);
-		MPI_Cart_coords(newcomm, newrank, ndim, coords);
+  if (rank < (dim * dim)) {
+    MPI_Comm_rank(newcomm, &newrank);
+    MPI_Cart_coords(newcomm, newrank, ndim, coords);
 
-		/* Create 1D row subgrids*/
-		remain_dims[0]=0;
-		remain_dims[1]=1;// belongs to subgrid
-		MPI_Cart_sub(newcomm, remain_dims, &Rcomm);
-		/* Create 1D column subgrids*/
-		remain_dims[0]=1;
-		remain_dims[1]=0;
-		MPI_Cart_sub(newcomm, remain_dims, &Ccomm);
+    /* Create 1D row subgrids*/
+    remain_dims[0] = 0;
+    remain_dims[1] = 1; // belongs to subgrid
+    MPI_Cart_sub(newcomm, remain_dims, &Rcomm);
+    /* Create 1D column subgrids*/
+    remain_dims[0] = 1;
+    remain_dims[1] = 0;
+    MPI_Cart_sub(newcomm, remain_dims, &Ccomm);
 
-		MPI_Comm_rank(Rcomm, &Rrank);
-		MPI_Comm_rank(Ccomm, &Crank);
-		printf("rank=%d, newrank=%d, (%d,%d), Rrank:%d Crank:%d\n", rank, newrank, coords[0], coords[1],Rrank,Crank);
+    MPI_Comm_rank(Rcomm, &Rrank);
+    MPI_Comm_rank(Ccomm, &Crank);
+    printf("rank=%d, newrank=%d, (%d,%d), Rrank:%d Crank:%d\n", rank, newrank,
+           coords[0], coords[1], Rrank, Crank);
+  }
 
-	}
-
-	if(newcomm != MPI_COMM_NULL)
-		MPI_Comm_free(&newcomm);
-	if(Rcomm != MPI_COMM_NULL)
-		MPI_Comm_free(&Rcomm);
-	if(Ccomm != MPI_COMM_NULL)
-		MPI_Comm_free(&Ccomm);
+  if (newcomm != MPI_COMM_NULL)
+    MPI_Comm_free(&newcomm);
+  if (Rcomm != MPI_COMM_NULL)
+    MPI_Comm_free(&Rcomm);
+  if (Ccomm != MPI_COMM_NULL)
+    MPI_Comm_free(&Ccomm);
 
   MPI_Finalize();
   return 0;
