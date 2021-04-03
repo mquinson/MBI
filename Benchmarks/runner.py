cfdac18d2a69d805375b5b32f8b312c69cc39a21
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-04-03 14:07:35 +0200

    Change again how we detect that SimGrid detected a memleak

--- Benchmarks/runner.py
+++ Benchmarks/runner.py
@@ -341,14 +341,14 @@ def simgridrun(execcmd, filename, binary, id, timeout, jobid):
         return res
     if re.search('DEADLOCK DETECTED', output):
         return 'deadlock'
-    if re.search('Leaked handle of type', output):
-        return 'resleak'
     if re.search('returned MPI_ERR', output):
         return 'mpierr'
     if re.search('Not yet implemented', output):
         return 'CUN'
     if re.search('CRASH IN THE PROGRAM', output):
         return 'segfault'
+    if re.search('Probable memory leaks in your code: SMPI detected', output):
+        return 'resleak'
     if re.search('No property violation found', output):
         return 'noerror'
 
