2b39011e2c89b72e913a3db0a9492f8f8ef2f1cb
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-04-18 00:31:59 +0200

    Split some utilities in a separate file for reuse

--- codes/templates/CollectiveOrdering.py
+++ codes/templates/CollectiveOrdering.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
-import re,sys
+import sys
+from generator_utils import make_file
 
 template = """// @{generatedby}@
 /* ///////////////////////// The MPI Bugs Initiative ////////////////////////
@@ -80,43 +81,6 @@ int main(int argc, char **argv) {
 }
 """
 
-def find_line(content, target, filename):
-  res = 1
-  for line in content.split('\n'):
-    if re.search(f'[^:]{target}', line):
-      #print(f'Found {target} at {line}')
-      return res
-    res += 1
-  raise Exception(f"Line target {target} not found in {filename}.")
-
-def make_file(filename, replace):
-  output = template
-  filename = re.sub("_MPI_","_", filename)
-  replace['filename'] = filename
-  # Replace all variables that don't have a ':' in their name
-  while re.search("@\{[^@:]*\}@", output):
-    m = re.search("@\{([^@:]*)\}@", output)
-    target = m.group(1)
-    #print(f"Replace @{{{target}}}@")
-    if target in replace.keys():
-      output = re.sub(f'@\{{{target}\}}@', replace[target], output)
-      #print(f"Replace {target} -> {replace[target]}")    
-    else:
-      raise Exception(f"Variable {target} used in template, but not defined.")
-  # Now replace all variables with a ':' in their name: line targets are like that, and we don't want to resolve them before the others change the lines
-  while re.search("@\{([^:@]*):([^@]*)\}@", output):
-    m = re.search("@\{([^:@]*):([^@]*)\}@", output)
-    (kind, target) = (m.group(1), m.group(2))
-    if kind == 'line':
-      replace = f'{find_line(output, target, filename)}'
-      #print(f"Replace @{{line:{target}}}@ with '{replace}'")
-      output = re.sub(f'@\{{line:{target}\}}@', replace, output)
-    else:
-      raise Exception(f"Unknown variable kind: {kind}:{target}")
-  # Ready to output it
-  with open(filename, 'w') as outfile:
-    outfile.write(output)
-
 collectives = ['MPI_Barrier', 'MPI_Reduce']#, 'allreduce']
 icollectives = []#'ibarrier', 'ireduce', 'iallreduce']
 
@@ -151,7 +115,7 @@ for coll1 in collectives + icollectives:
       replace['longdesc'] = f'All ranks call {coll1} twice'
       replace['outcome'] = 'OK'
       replace['errormsg'] = ''
-      make_file(f'CollectiveOrder_{coll1}_{coll2}.c', replace)
+      make_file(template, f'CollectiveOrder_{coll1}_{coll2}.c', replace)
     else: 
       # Generate the correct ordering
       replace = patterns
@@ -159,7 +123,7 @@ for coll1 in collectives + icollectives:
       replace['longdesc'] = f'All ranks call {coll1} and then {coll2}'
       replace['outcome'] = 'OK'
       replace['errormsg'] = ''
-      make_file(f'CollectiveOrder_{coll1}_{coll2}_ok.c', replace)
+      make_file(template, f'CollectiveOrder_{coll1}_{coll2}_ok.c', replace)
 
       # Generate the incorrect ordering
       replace = patterns
@@ -170,4 +134,4 @@ for coll1 in collectives + icollectives:
 
       replace['operation1b'] = operation[coll1]("2") # Inversion
       replace['operation2b'] = operation[coll2]("1")
-      make_file(f'CollectiveOrder_{coll1}_{coll2}_nok.c', replace)
+      make_file(template, f'CollectiveOrder_{coll1}_{coll2}_nok.c', replace)
