ee60b71bca321243e52af1a9bfa7c80e3f1d6e77
Emmanuelle Saillard <emmanuelle.saillard@inria.fr>
2021-04-19 21:31:21 +0200

    add a script for communicator matching

--- codes/templates/CollectiveOrdering.py
+++ codes/templates/CollectiveOrdering.py
@@ -44,10 +44,11 @@ END_MBI_TESTS
 #include <mpi.h>
 #include <stdio.h>
 
+#define buff_size 128
+
 int main(int argc, char **argv) {
   int nprocs = -1;
   int rank = -1;
-  int localsum, sum;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -58,11 +59,9 @@ int main(int argc, char **argv) {
     printf("\\033[0;31m! This test needs at least 2 processes to produce a bug "
            "!\\033[0;0m\\n");
 
-  localsum = 0;
-  for (int i = 0; i <= rank; i++) {
-    localsum += i;
-  }
-  printf("process %d has local sum of %d\\n", rank, localsum);
+
+	@{init1}@
+	@{init2}@
 
   if (rank % 2) {
     @{operation1a}@ /* MBIERROR1 */
@@ -72,16 +71,13 @@ int main(int argc, char **argv) {
     @{operation2b}@
   }
 
-  if (rank == 0)
-    printf("total sum is %d\\n", sum);
-
   MPI_Finalize();
   printf("\\033[0;32mrank %d Finished normally\\033[0;0m\\n", rank);
   return 0;
 }
 """
 
-collectives = ['MPI_Barrier', 'MPI_Bcast', 'MPI_Reduce', 'MPI_Allreduce']
+collectives = ['MPI_Barrier', 'MPI_Bcast', 'MPI_Reduce', 'MPI_Allreduce', 'MPI_Scatter', 'MPI_Gather']
 icollectives = []#'ibarrier', 'ireduce', 'iallreduce']
 
 init = {}
@@ -98,6 +94,13 @@ operation['MPI_Reduce'] = lambda n: f"MPI_Reduce(&sum{n}, &val{n}, 1, MPI_INT, M
 
 init['MPI_Allreduce'] = lambda n: f"int sum{n}, val{n} = 1;"
 operation['MPI_Allreduce'] = lambda n: f"MPI_Allreduce(&sum{n}, &val{n}, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);"
+
+init['MPI_Scatter'] = lambda n: f"int val{n}, buf{n}[buff_size];"
+operation['MPI_Scatter'] = lambda n: f"MPI_Scatter(&buf{n}, 1, MPI_INT, &val{n}, 1, MPI_INT, root, MPI_COMM_WORLD);"
+
+init['MPI_Gather'] = lambda n: f"int val{n}, buf{n}[buff_size];"
+operation['MPI_Gather'] = lambda n: f"MPI_Gather(&val{n}, 1, MPI_INT, buf{n},1, MPI_INT, root, MPI_COMM_WORLD);"
+
 
 
 for coll1 in collectives + icollectives:
@@ -138,7 +141,6 @@ for coll1 in collectives + icollectives:
     	replace['longdesc'] = f'Odd ranks call {coll1} and then {coll2} while even ranks call these collectives in the other order'
     	replace['outcome'] = 'ERROR: CollectiveOrdering'
     	replace['errormsg'] = 'Collective mistmatch. @{coll1}@ at @{filename}@:@{line:MBIERROR1}@ is matched with @{coll2}@ line @{filename}@:@{line:MBIERROR2}@.'
-
     	replace['operation1b'] = operation[coll2]("1") # Inversion
     	replace['operation2b'] = operation[coll1]("2")
     	make_file(template, f'CollCallOrder_{coll1}_{coll2}_nok.c', replace)
