ff60dbc1a70cc7ad770ceb8b5db1876b088508a2
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-05-28 23:15:04 +0200

    stupid me

--- scripts/must.py
+++ scripts/must.py
@@ -26,8 +26,8 @@ class Tool(AbstractTool):
     def run(self, execcmd, filename, binary, id, timeout):
         cachefile = f'{binary}_{id}'
 
-        execcmd = re.sub("/builds/MUST/bin/mpirun", "mustrun --must:distributed", execcmd)
-        execcmd = re.sub('\${EXE}', binary, execcmd)
+        execcmd = re.sub("mpirun", "/builds/MUST/bin/mustrun --must:distributed", execcmd)
+        execcmd = re.sub('\${EXE}', f'./{binary}', execcmd)
         execcmd = re.sub('\$zero_buffer', "", execcmd)
         execcmd = re.sub('\$infty_buffer', "", execcmd)
 
