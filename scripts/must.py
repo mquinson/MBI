61f62e43fadcd7b77cd29a8efc43d641fc95fc53
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-05-29 13:08:36 +0200

    Try to fix the MUST runs :sob:

--- scripts/must.py
+++ scripts/must.py
@@ -26,7 +26,7 @@ class Tool(AbstractTool):
     def run(self, execcmd, filename, binary, id, timeout, batchinfo):
         cachefile = f'{binary}_{id}'
 
-        execcmd = re.sub("mpirun", "/MBI/builds/MUST/bin/mustrun --must:distributed", execcmd)
+        execcmd = re.sub("mpirun", "mustrun --must:distributed", execcmd)
         execcmd = re.sub('\${EXE}', f'./{binary}', execcmd)
         execcmd = re.sub('\$zero_buffer', "", execcmd)
         execcmd = re.sub('\$infty_buffer', "", execcmd)
