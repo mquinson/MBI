2fff118d095254116e21fbc2da6b8f6d2bf9b4a5
Pierre-Antoine Rouby <pierre-antoine.a.rouby@inria.fr>
2022-04-21 15:47:11 +0200

    generator: Buffering hazard add circular communication code.

--- scripts/P2PBufferingGenerator.py
+++ scripts/P2PBufferingGenerator.py
@@ -22,10 +22,10 @@ BEGIN_MPI_FEATURES
 END_MPI_FEATURES
 
 BEGIN_MBI_TESTS
-  $ mpirun -np 2 $zero_buffer ${EXE}
+  $ mpirun -np 4 $zero_buffer ${EXE}
   | @{outcome1}@
   | @{errormsg1}@
-  $ mpirun -np 2 $infty_buffer ${EXE}
+  $ mpirun -np 4 $infty_buffer ${EXE}
   | @{outcome1}@
   | @{errormsg1}@
 END_MBI_TESTS
@@ -48,8 +48,8 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   printf("Hello from rank %d \\n", rank);
 
-  if (nprocs < 2)
-    printf("MBI ERROR: This test needs at least 2 processes to produce a bug!\\n");
+  if (nprocs < 4)
+    printf("MBI ERROR: This test needs at least 4 processes to produce a bug!\\n");
 
   MPI_Comm newcom = MPI_COMM_WORLD;
   MPI_Datatype type = MPI_INT;
@@ -68,6 +68,12 @@ int main(int argc, char **argv) {
     @{fini1b}@
     @{operation2b}@
     @{fini2b}@
+  }else{
+    src=@{src3}@,dest=@{dest3}@;
+    @{operation1c}@
+    @{fini1c}@
+    @{operation2c}@
+    @{fini2c}@
   }
   @{free1}@
   @{free2}@
@@ -92,18 +98,24 @@ for s in send + isend:
         patterns['dest1'] = '1'
         patterns['src2'] = '0'
         patterns['dest2'] = '0'
+        patterns['src3'] = '0'
+        patterns['dest3'] = '0'
         patterns['init1'] = init[s]("1")
         patterns['init2'] = init[r]("2")
         patterns['fini1a'] = fini[s]("1")
         patterns['fini1b'] = fini[s]("1")
+        patterns['fini1c'] = ''
         patterns['fini2a'] = fini[r]("2")
         patterns['fini2b'] = fini[r]("2")
+        patterns['fini2c'] = ''
         patterns['free1'] = free[s]("1")
         patterns['free2'] = free[r]("2")
         patterns['operation1a'] = operation[s]("1")
         patterns['operation2a'] = operation[r]("2")
         patterns['operation1b'] = operation[s]("1")
         patterns['operation2b'] = operation[r]("2")
+        patterns['operation1c'] = ''
+        patterns['operation2c'] = ''
 
         # Generate the incorrect matching depending on the buffering mode (send + recv)
         replace = patterns
@@ -125,6 +137,25 @@ for s in send + isend:
         replace['outcome1'] = 'ERROR: BufferingHazard'
         replace['errormsg1'] = f'Buffering Hazard. Possible deadlock depending the buffer size of MPI implementation and system environment cause Send message to the same process.'
         make_file(template, f'P2PBuffering_SameProcess_{s}_{r}_nok.c', replace)
+
+        # Generate the incorrect matching with circular send message depending on the buffering mode (send + recv)
+        replace = patterns.copy()
+        replace['origin'] = 'RTED'
+        replace['src1'] = '(nprocs - 1)'
+        replace['dest1'] = '1'
+        replace['src2'] = '0'
+        replace['dest2'] = '2'
+        replace['src3'] = '(rank - 1)'
+        replace['dest3'] = '((rank + 1) % nprocs)'
+        replace['fini1c'] = fini[r]("1")
+        replace['fini2c'] = fini[r]("2")
+        replace['operation1c'] = operation[s]("1") + ' /* MBIERROR3 */'
+        replace['operation2c'] = operation[r]("2")
+        replace['shortdesc'] = 'Point to point @{s}@ and @{r}@ may not be matched'
+        replace['longdesc'] = 'Processes 0 and 1 both call @{s}@ and @{r}@. This results in a deadlock depending on the buffering mode'
+        replace['outcome1'] = 'ERROR: BufferingHazard'
+        replace['errormsg1'] = f'Buffering Hazard. Possible deadlock depending the buffer size of MPI implementation and system environment cause circular send message.'
+        make_file(template, f'P2PBuffering_Circular_{s}_{r}_nok.c', replace)
 
         # Generate the incorrect matching depending on the buffering mode (recv + send)
         replace = patterns
